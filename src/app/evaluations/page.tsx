275651fed0c308a1d9d974ded3718f5c0d9ea998
ZhaoXinyu <2416715020@qq.com>
2025-12-02 12:20:26 +0800

    context load failed

--- src/app/evaluations/page.tsx
+++ src/app/evaluations/page.tsx
@@ -1,10 +1,16 @@
+'use client';
 import { EvaluationWorkspace } from "@/components/evaluations/evaluation-workspace";
+import { EvaluationVersion } from "@/lib/evaluations/models";
 import { fetchEvaluationTree } from "@/lib/evaluations/repository";
+import { useEffect, useState } from "react";
 
 export const dynamic = "force-dynamic";
 
 export default async function EvaluationsPage() {
-    const versions = await fetchEvaluationTree();
+    const [versions, setVersions] = useState<EvaluationVersion[]>([]);
+    useEffect(() => {
+        fetchEvaluationTree().then(setVersions);
+    }, []);
 
     return (
         <div className="flex h-[calc(100vh-4rem)] flex-col pb-6">
